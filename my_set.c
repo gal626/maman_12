3e31f8d4d59d030a7fd98388bd25d02b73daacc2
gitzhak <gal.itzhak@emedgene.com>
2023-03-11 16:28:18 +0200

    add input validation

--- my_set.c
+++ my_set.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <ctype.h>
 
 /* TODO: change size */
 #define ENLARGE_SIZE 2
 
-int contains(const int *ptr, int current_size, int n){
+int contains(const int *ptr, int current_size, int num){
     int k;
     for(k = 0; k < current_size; k++)
-        if(ptr[k] == n){
+        if(ptr[k] == num){
             return 1;
         }
     return 0;
@@ -15,24 +16,34 @@ int contains(const int *ptr, int current_size, int n){
 
 int * get_set(int *length_of_inputs){
     int *ptr, size, num;
-
+    char c;
     size = ENLARGE_SIZE;
     /*allocate a memory for integers in amount of size*/
     ptr = malloc(size * sizeof(int));
 
     printf("insert_numbers\n");
-    while(scanf("%d", &num) != EOF ){
-        if(!contains(ptr, *length_of_inputs, num)){
-            if (*length_of_inputs == size){
-                size+=ENLARGE_SIZE;
-                ptr = realloc(ptr, size * sizeof(int));
-                printf("size is %d\n", size); /* TODO: only for debug */
+    while ((c = getchar()) != EOF){
+        if (isdigit(c)) {
+            num = c - '0';
+            while (isdigit(c = getchar())) {
+                num = num * 10 + (c - '0');
             }
-            *(ptr + *length_of_inputs) = num;
-            *length_of_inputs+=1;
+            if(!contains(ptr, *length_of_inputs, num))
+            {
+                if (*length_of_inputs == size){
+                    size+=ENLARGE_SIZE;
+                    ptr = realloc(ptr, size * sizeof(int));
+                    printf("size is %d\n", size); /* TODO: only for debug */
+                }
+                *(ptr + *length_of_inputs) = num;
+                *length_of_inputs+=1;
+            }
+            else{
+                printf("%d already in set\n", num); /* TODO: only for debug */
+            }
         }
-        else{
-            printf("%d already in set\n", num); /* TODO: only for debug */
+        if(!(isspace(c) || c == '\n' || c == EOF)){
+            printf("ignoring invalid character %c", c);
         }
     }
     return ptr;
