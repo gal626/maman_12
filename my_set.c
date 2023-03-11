230c1da6c8e52e819259395b8a19e33daff86764
gitzhak <gal.itzhak@emedgene.com>
2023-03-11 16:37:13 +0200

    add comments

--- my_set.c
+++ my_set.c
@@ -5,6 +5,7 @@
 /* TODO: change size */
 #define ENLARGE_SIZE 2
 
+/* checks if num is in the set starting from the pointer ptr */
 int contains(const int *ptr, int current_size, int num){
     int k;
     for(k = 0; k < current_size; k++)
@@ -14,6 +15,8 @@ int contains(const int *ptr, int current_size, int num){
     return 0;
 }
 
+/* create a set from input of numbers until getting EOF
+ * This functions ignores non digit input*/
 int * get_set(int *length_of_inputs){
     int *ptr, size, num;
     char c;
@@ -31,10 +34,12 @@ int * get_set(int *length_of_inputs){
             if(!contains(ptr, *length_of_inputs, num))
             {
                 if (*length_of_inputs == size){
+                    /* enlarge the set length by ENLARGE_SIZE elements*/
                     size+=ENLARGE_SIZE;
                     ptr = realloc(ptr, size * sizeof(int));
                     printf("size is %d\n", size); /* TODO: only for debug */
                 }
+                /* insert num to the set */
                 *(ptr + *length_of_inputs) = num;
                 *length_of_inputs+=1;
             }
@@ -43,12 +48,13 @@ int * get_set(int *length_of_inputs){
             }
         }
         if(!(isspace(c) || c == '\n' || c == EOF)){
-            printf("ignoring invalid character %c", c);
+            printf("ignoring invalid character %c\n", c);
         }
     }
     return ptr;
 }
 
+/* print 'length_of_inputs' elements starting from the pointer ptr */
 void print_set(int *ptr, int length_of_inputs){
     //printing all input numbers
     int i;
@@ -58,7 +64,7 @@ void print_set(int *ptr, int length_of_inputs){
 
 int main()
 {
-    /* number of integers received from the input*/
+    /* number of integers received from the input, initialized to 0*/
     int length_of_inputs;
     length_of_inputs = 0;
     int *ptr = get_set(&length_of_inputs);
