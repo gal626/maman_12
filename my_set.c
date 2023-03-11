dc6f66180b7c230a786eaa5f0853fce912dfab51
gitzhak <gal.itzhak@emedgene.com>
2023-03-11 16:48:01 +0200

    print input numbers

--- my_set.c
+++ my_set.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include <ctype.h>
 
 /* TODO: change size */
 #define ENLARGE_SIZE 2
@@ -19,36 +18,27 @@ int contains(const int *ptr, int current_size, int num){
  * This functions ignores non digit input*/
 int * get_set(int *length_of_inputs){
     int *ptr, size, num;
-    char c;
+
     size = ENLARGE_SIZE;
     /*allocate a memory for integers in amount of size*/
     ptr = malloc(size * sizeof(int));
 
     printf("insert_numbers\n");
-    while ((c = getchar()) != EOF){
-        if (isdigit(c)) {
-            num = c - '0';
-            while (isdigit(c = getchar())) {
-                num = num * 10 + (c - '0');
+    while(scanf("%d", &num) != EOF ){
+        printf("input: %d\n", num);
+        if(!contains(ptr, *length_of_inputs, num)){
+            if (*length_of_inputs == size){
+                /* enlarge the set length by ENLARGE_SIZE elements*/
+                size+=ENLARGE_SIZE;
+                ptr = realloc(ptr, size * sizeof(int));
+                printf("size is %d\n", size); /* TODO: only for debug */
             }
-            if(!contains(ptr, *length_of_inputs, num))
-            {
-                if (*length_of_inputs == size){
-                    /* enlarge the set length by ENLARGE_SIZE elements*/
-                    size+=ENLARGE_SIZE;
-                    ptr = realloc(ptr, size * sizeof(int));
-                    printf("size is %d\n", size); /* TODO: only for debug */
-                }
-                /* insert num to the set */
-                *(ptr + *length_of_inputs) = num;
-                *length_of_inputs+=1;
-            }
-            else{
-                printf("%d already in set\n", num); /* TODO: only for debug */
-            }
+            /* insert num to the set */
+            *(ptr + *length_of_inputs) = num;
+            *length_of_inputs+=1;
         }
-        if(!(isspace(c) || c == '\n' || c == EOF)){
-            printf("ignoring invalid character %c\n", c);
+        else{
+            printf("%d already in set\n", num); /* TODO: only for debug */
         }
     }
     return ptr;
@@ -57,9 +47,12 @@ int * get_set(int *length_of_inputs){
 /* print 'length_of_inputs' elements starting from the pointer ptr */
 void print_set(int *ptr, int length_of_inputs){
     //printing all input numbers
+    printf("set is: (");
     int i;
-    for(i = 0; i < length_of_inputs; i++)
-        printf("%d\n",ptr[i]);
+    for(i = 0; i < length_of_inputs-1; i++)
+        printf("%d, ",ptr[i]);
+    printf("%d)", ptr[i]);
+
 }
 
 int main()
